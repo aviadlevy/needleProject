3a453b40f008246773446295b44aa94f54261bd3
Aviad Levy <aviadlevy1@gmail.com>
2017-02-12 17:23:45 +0200

    more stability

--- parser.py
+++ parser.py
@@ -43,22 +43,32 @@ def parse_single_page(url_suff):
     # rating scale (max rating possible)
     rating_scale = float(res_html.find("meta", property="og:rating_scale")["content"])
     # total time (prep + cook)
-    ready_in_time_str = res_html.find("span", {
-        "class": "ready-in-time"
-    }).text
-    ready_in_time_min = str_to_min(ready_in_time_str.lower())
+    try:
+        ready_in_time_str = res_html.find("span", {
+            "class": "ready-in-time"
+        }).text
+        ready_in_time_min = str_to_min(ready_in_time_str.lower())
+    except:
+        ready_in_time_min = None
     servings = float(res_html.find("meta", id="metaRecipeServings")["content"])
-    calories = float(res_html.find("span", {
+    try:
+        calories = float(res_html.find("span", {
         "class": "calorie-count"
     }).find("span").text)
+    except:
+        calories = None
     ingredients = []
     for col in res_html.find_all("ul", id=lambda x: x and x.startswith("lst_ingredients_")):
         for ing in col.find_all("li"):
             if "Add all ingredients to list" not in ing.text:
                 ingredients.append(ing.text.replace("ADVERTISEMENT", "").strip())
-    prep_time_str = res_html.find("time", itemprop="prepTime").text
-    prep_time_min = str_to_min(prep_time_str.lower())
-    cook_time_min = ready_in_time_min - prep_time_min
+    try:
+        prep_time_str = res_html.find("time", itemprop="prepTime").text
+        prep_time_min = str_to_min(prep_time_str.lower())
+        cook_time_min = ready_in_time_min - prep_time_min
+    except:
+        prep_time_min = None
+        cook_time_min = None
     directions = []
     for step in res_html.find_all("li", {
         "class": "step"
@@ -111,11 +121,11 @@ if __name__ == '__main__':
     for i in range(int(page_num), int(page_end)):
         recipes_list = get_recipes_from_page(i)
         if not recipes_list:
-            break
+            continue
         for page in recipes_list:
             ret = parse_single_page(page)
-            with open("data", "a") as f:
+            with open("real_data", "a") as f:
                 f.write(json.dumps(ret) + "\n")
-            sleep(1)
+            sleep(15)
         with open("savestate", "w") as f:
             f.write("last page parsed is: " + str(i))
